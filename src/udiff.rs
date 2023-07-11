9f76d88c3d3b583b01814d9e47cdbb4b6ea8f2cd
Tshepang Mbambo <tshepang@gmail.com>
2023-07-11 22:26:06 +0200

    fix some english things (#48)

--- src/udiff.rs
+++ src/udiff.rs
@@ -149,8 +149,8 @@ impl<'diff, 'old, 'new, 'bufs, T: DiffableStr + ?Sized> UnifiedDiff<'diff, 'old,
     ///
     /// `a` and `b` are the file names that are added to the top of the unified
     /// file format.  The names are accepted verbatim which lets you encode
-    /// a timestamp into it when separated by a tab (`\t`).  For more information
-    /// see [the unified diff format specification](https://pubs.opengroup.org/onlinepubs/9699919799/utilities/diff.html#tag_20_34_10_07)
+    /// a timestamp into it when separated by a tab (`\t`).  For more information,
+    /// see [the unified diff format specification](https://pubs.opengroup.org/onlinepubs/9699919799/utilities/diff.html#tag_20_34_10_07).
     pub fn header(&mut self, a: &str, b: &str) -> &mut Self {
         self.header = Some((a.to_string(), b.to_string()));
         self
